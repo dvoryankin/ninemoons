47404b6ab46b0a588154a28140ba31490e4b30dd
Dmitry <dvoryankindm@gmail.com>
2018-10-11 22:48:01 +0500

    make sign_in root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'index#index'
+  
+  root :to => redirect("/users/sign_in")
 end
