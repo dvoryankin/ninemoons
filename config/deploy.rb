2e9821ff859b223438f57f06ab498a730ec462a9
dvoryankin <dvoryankindm@gmail.com>
2018-11-15 16:03:50 +0500

    add linked_files

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
-#set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs,  %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle }
 
 
