dcce88e327470e1033fac3a0d3b7fa380ed0189d
dvoryankin <dvoryankindm@gmail.com>
2018-11-15 20:48:41 +0500

    cookie try

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
-  before_action :set_title, :register_visitor, :first_time_visit, unless: -> { cookies[:visitor_id] }
+  before_action :set_title, :register_visitor, unless: -> { cookies[:visitor_id] }
 
   def register_visitor
+    @name = Faker::StarWars.character
+
     if cookies[:visitor_id].present?
       @visitor = Visitor.find_by(id: cookies.signed_or_encrypted[Rails.application.secrets.visitor_cookie_name])
 
@@ -15,16 +17,16 @@ class ApplicationController < ActionController::Base
                                  referrer: request.referrer,
                                  visitor_id: cookies[:visitor_id],
                                  user_id: user_id,
-                                 name: Faker::StarWars.character)
+                                 name: @name)
       # cookies.signed_or_encrypted.permanent[Rails.application.secrets.visitor_cookie_name] = @visitor.id
 
     end
   end
 
-  def first_time_visit
-    cookies.permanent[:visitor_id] = SecureRandom.hex(8)
-    @first_visit = true
-  end
+  # def first_time_visit
+  #   cookies.permanent[:visitor_id] = SecureRandom.hex(8)
+  #   @first_visit = true
+  # end
 
   def set_title
     @title = params[:action]
